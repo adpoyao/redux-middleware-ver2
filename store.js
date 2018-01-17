69f5cb2ac768b85eb8f814d0ae96a9ae35089fd8
Eddie Yao <adpoyao@gmail.com>
2018-01-17 10:39:48 -0800

    Prepares file for Nate to sync.;

--- store.js
+++ store.js
@@ -8,18 +8,14 @@ import { reducer as incrementReducer } from './reducers/increment';
 import { helloWorld, startIncrement, increment, pause } from './actions';
 import { HELLO_WORLD, START_INCREMENT, INCREMENT, PAUSE } from './actions/actionType';
 
-function createDelayMiddleware(extraArg) {
-  return store => next => action => {
+export const delayMiddleware = ({ dispatch, getState }) => next => action => {
     if (action.type === START_INCREMENT) {
       let interval = setInterval(() => store.dispatch(increment()), 1000);
     } else if (action.type === PAUSE) {
       clearInterval(interval);
     }
     return next(action);
-  }
 }
-
-export const delayMiddleware = createDelayMiddleware();
 
 const store = createStore(
   combineReducers({
--- test/store.test.js
+++ test/store.test.js
@@ -4,19 +4,23 @@ import store, { delayMiddleware } from '../store';
 import { startIncrement, increment, pause } from '../actions';
 import { START_INCREMENT, INCREMENT, PAUSE } from '../actions/actionType';
 
-describe('delayMiddelware', () => {
-  const doDispatch = () => {};
-  const doGetState = () => {};
-  const nextHandler = delayMiddleware(store);
-
-  it('returns a function to handle next', () => {
-    expect(nextHandler).toHaveLength(1);
+describe('delayMiddleware', () => {
+  const create = () => {
+    const store = {
+    getState: jest.fn(() => ({})),
+    dispatch: jest.fn(),
+    };
+    const next = jest.fn()
     
+    const invoke = (action) => delayMiddleware(store)(next)(action)
+    
+    return {store, next, invoke}
+  };
 
-    // delayMiddleware(store)(next)(increment())
-    // return setTimeout(function(){return pause()}, 2000)
-    // .then(result => {
-    //   expect(result).toEqual(2)
-    // });
-  })
+  it('passed through non-function action', () => {
+    const { next, invoke } = create()
+    const action = {type: 'START_INCREMENT'}
+    invoke(action)
+    expect(next).toHaveBeenCalledWith(action)
+  });
 });
